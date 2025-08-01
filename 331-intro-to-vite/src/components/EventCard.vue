8a02d97bf8604d707c39251ac484200e33cd13b5
poryasin <poraya_sin@cmu.ac.th>
2025-08-01 11:36:07 +0700

    Lab5.5

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -22,13 +22,14 @@ import {type Event } from '@/types'
 
 <template>
   <RouterLink class="event-link" :to="{name: 'event-detail-view',params:{ id: event.id} }">
-    <div class="event-card">
+    <div class="cursor-pointer border border-gray-600 p-4 w-64 mb-6">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
-    </div>
       <div class="category">
     <CategoryInfo  :event="event" />
   </div>
+    </div>
+      
   </RouterLink>
 </template>
 
