d919efacd9906aa9b60edbe1d57b2c7affd793d4
poryasin <poraya_sin@cmu.ac.th>
2025-08-08 15:56:53 +0700

    edit main.ts

--- 331-intro-to-vite/src/main.ts
+++ 331-intro-to-vite/src/main.ts
@@ -6,7 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
+import { Analytics } from "@vercel/analytics/next"
 
+// Pass an empty object or appropriate props if needed
+Analytics({})
 const app = createApp(App)
 
 app.use(createPinia())
